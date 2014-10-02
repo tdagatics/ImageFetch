6c166d4e9dcddf242d12fa06c70cab642584dbae
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-01 21:03:04 -0700

    Final commit of ImageFetch

--- ImageFetch/main.m
+++ ImageFetch/main.m
@@ -14,8 +14,31 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSURL *url = [NSURL URLWithString:@"http://www.google.com/images/logos/ps_logo2.png"];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
         
+        NSError *error = nil;
+        NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:NULL error:&error];
+        
+        if (!data) {
+            NSLog(@"Fetch failed; %@", [error description]);
+            return 1;
+        }
+        
+        NSLog(@"The file is %lu bytes", (unsigned long) [data length]);
+        
+        BOOL written = [data writeToFile:@"/tmp/google.png" options:NSDataWritingAtomic error:&error];
+        
+        if (!written) {
+            NSLog(@"write failed: %@", [error localizedDescription]);
+            return 1;
+        }
+        
+        NSLog(@"Success!");
+
+        NSData *readData = [NSData dataWithContentsOfFile:@"/tmp/google.png"];
+        NSLog(@"The file read from the disk has %lu bytes", (unsigned long) [readData length]);
+    
     }
     return 0;
 }
